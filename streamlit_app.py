201daf266b23e935589d2d9b72a8b7c24faa2bd0
Manish Kumar <119120517+manish4993@users.noreply.github.com>
2024-01-18 15:43:05 +0530

    Update streamlit_app.py
    deleted raw json line

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 # taking the json version of data and normalizing it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
